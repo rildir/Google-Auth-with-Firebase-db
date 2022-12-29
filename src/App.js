8e6b7b5e098c96cdf2ca9d8291899a8bf499b939
rildir <recepildir95@gmail.com>
2022-12-29 16:02:20 +0300

    auth

--- src/App.js
+++ src/App.js
@@ -1,39 +1,27 @@
-// import UsersItem from "./components/category-item/users-item.components";
-// import Directory from "./components/directory/directory.component";
+import { Routes, Route } from "react-router-dom";
 
-import CategoryItem from "./components/category-item/category-item.component";
-import Directory from "./components/directory/directory.component";
+import Navigation from "./routes/navigation/navigation.component";
+import Home from "./routes/home/home.component";
+import SignIn from "./routes/sign-in/sign-in.component";
+
+const Shop = () => {
+  return (
+    <div>
+      <h2>I am the shop page</h2>
+    </div>
+  );
+};
 
 function App() {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
-  return <Directory categories={categories} />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,40 @@
+import { initializeApp } from "firebase/app";
+
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB34lpM6xuNw1cFMBa10ONiKB6DnkMFCPw",
+  authDomain: "crwn-clothing-a9aa9.firebaseapp.com",
+  projectId: "crwn-clothing-a9aa9",
+  storageBucket: "crwn-clothing-a9aa9.appspot.com",
+  messagingSenderId: "566395920587",
+  appId: "1:566395920587:web:ff52ec0d2e58cce1147b4e",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+};
